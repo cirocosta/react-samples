14a6a83cd9f364f1b654f35aa0daecfdcd8fe694
Ciro S. Costa <ciro.costa@usp.br>
2014-09-07 01:18:06 -0300

    (jest) updates preprocessor

--- jest/utils/jsx-preprocessor.js
+++ jest/utils/jsx-preprocessor.js
@@ -1,7 +1,10 @@
 var ReactTools = require('react-tools');
 
 module.exports = {
-  process: function (src) {
+  process: function (src, path) {
+    if (!path.match(/\.jsx$/))
+      return src;
+
     return ReactTools.transform(src);
   }
 };
